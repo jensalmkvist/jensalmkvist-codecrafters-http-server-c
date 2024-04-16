f350161ff502ed0c7a709ab0487e7722fc0c1c80
Jens Thieme Almkvist <jens.thieme-almkvist@br-automation.com>
2024-04-16 13:19:46 +0200

    testing to see if i can detect /echo

--- app/server.c
+++ app/server.c
@@ -128,7 +128,7 @@ int main()
 	{
 		send(client_fd, HTTP_status_codes.HTTP_OK, sizeof(HTTP_status_codes.HTTP_OK), 0); // send response to client
 	}
-	else if(strcmp(request.path, "/echo/"))
+	else if(strstr(request.path, "/echo/") != NULL)
 	{
 		printf("Printing path\n");
 		printf(request.path);
