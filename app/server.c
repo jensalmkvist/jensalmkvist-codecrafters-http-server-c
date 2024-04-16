29937dcd49f806e65edd2bd0e5cae84f2841e774
Jens Thieme Almkvist <jens.thieme-almkvist@br-automation.com>
2024-04-16 16:39:29 +0200

    testing

--- app/server.c
+++ app/server.c
@@ -127,7 +127,7 @@ int main()
 	//	printf("Path: %s\n", request.path);
 	//	printf("HTTP Protocol: %s\n", request.http_protocol);
 
-	char responseStr[2048] = "";
+	char responseStr[1024];
 
 	if (strcmp(request.path, "/") == 0) // check if string is only /
 	{
