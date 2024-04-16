7ae82d1bf4f539bb793406dc0d0841a70e4618f3
Jens Thieme Almkvist <jens.thieme-almkvist@br-automation.com>
2024-04-16 11:02:16 +0200

    testing

--- app/server.c
+++ app/server.c
@@ -8,82 +8,82 @@
 #include <unistd.h>
 #include <server_variables.h>
 
-
-
-int main() {
-	//variable declarations
-	struct HTTP_status_codes {
-    /* data */
-    const char HTTP_NOT_FOUND[30] = "HTTP/1.1 404 Not Found\r\n\r\n";
-    const char HTTP_OK[30] = "HTTP/1.1 200 OK\r\n\r\n";
-};
+int main()
+{
+	// variable declarations
+	struct HTTP_status_codes
+	{
+		const char HTTP_NOT_FOUND[30] = "HTTP/1.1 404 Not Found\r\n\r\n";
+		const char HTTP_OK[30] = "HTTP/1.1 200 OK\r\n\r\n";
+	};
 
 	// Disable output buffering
 	setbuf(stdout, NULL);
-
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
 	// Uncomment this block to pass the first stage
-	
+
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
-	
+
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
+	if (server_fd == -1)
+	{
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return 1;
 	}
-	
+
 	// Since the tester restarts your program quite often, setting REUSE_PORT
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+	{
 		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
 		return 1;
 	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(4221),
+		.sin_addr = {htonl(INADDR_ANY)},
+	};
+
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+	{
 		printf("Bind failed: %s \n", strerror(errno));
 		return 1;
 	}
-	
+
 	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
+	if (listen(server_fd, connection_backlog) != 0)
+	{
 		printf("Listen failed: %s \n", strerror(errno));
 		return 1;
 	}
-	
+
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
-	
 
 	// Stage 2 code
-	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len); //create file descriptor for client
+	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len); // create file descriptor for client
 	printf("Client connected\n");
 
-	//decalre buffer for client message
+	// decalre buffer for client message
 	char client_buffer[1024];
 
-	if(read(client_fd, client_buffer, sizeof(client_buffer)) < 0) { //read from client
+	if (read(client_fd, client_buffer, sizeof(client_buffer)) < 0)
+	{ // read from client
 		printf("Error reading from client: %s\n", strerror(errno));
 		return -1;
 	}
 
-	//const char HTTP_OK[] = "HTTP/1.1 200 OK\r\n\r\n";
-	send(client_fd, HTTP_status_codes.HTTP_OK, sizeof(HTTP_status_codes.HTTP_OK), 0); //send response to client
+	// const char HTTP_OK[] = "HTTP/1.1 200 OK\r\n\r\n";
+	send(client_fd, HTTP_status_codes.HTTP_OK, sizeof(HTTP_status_codes.HTTP_OK), 0); // send response to client
 
-	//stage 3 code
-	const char HTTP_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-
-	
+	// stage 3 code
+	printf(HTTP_status_codes.HTTP_OK);
 
 	close(server_fd);
 
