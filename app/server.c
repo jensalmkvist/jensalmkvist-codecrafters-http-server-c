244774c18e568f49179793229149923e0101f40d
Jens Thieme Almkvist <jens.thieme-almkvist@br-automation.com>
2024-04-18 16:05:53 +0200

    testing

--- app/server.c
+++ app/server.c
@@ -30,143 +30,224 @@ int main(int argc, char *argv[])
 	{
 		char HTTP_NOT_FOUND[30];
 		char HTTP_OK[30];
+		char HTTP_CREATED[30];
 	};
 
 	struct HTTP_STATUS_CODES HTTP_status_codes = {
 		"HTTP/1.1 404 Not Found\r\n",
 		"HTTP/1.1 200 OK\r\n"};
+	"HTTP/1.1 201 Created\r\n"
+};
 
-	struct HTTP_STATUS_MESSAGES
+struct HTTP_STATUS_MESSAGES
+{
+	char NOT_FOUND[30];
+	char OK[30];
+	char CREATED[30];
+};
+
+struct HTTP_STATUS_MESSAGES HTTP_status_messages = {
+	"404 Not Found",
+	"200 OK"
+	"201 Created"};
+
+struct Request
+{
+	char http_method[10];
+	char path[100];
+	char http_protocol[10];
+};
+
+struct Request request;
+
+struct Response
+{
+	char http_protocol[10];
+	char status_code[30];
+	char status_message[30];
+	char content_type[30];
+	char content_length[10];
+	char body[1000];
+};
+
+struct Response response = {
+	.http_protocol = "HTTP/1.1"};
+
+// Disable output buffering
+setbuf(stdout, NULL);
+
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+printf("Logs from your program will appear here!\n");
+
+// Uncomment this block to pass the first stage
+
+int server_fd, client_addr_len;
+struct sockaddr_in client_addr;
+
+server_fd = socket(AF_INET, SOCK_STREAM, 0);
+if (server_fd == -1)
+{
+	printf("Socket creation failed: %s...\n", strerror(errno));
+	return 1;
+}
+
+// Since the tester restarts your program quite often, setting REUSE_PORT
+// ensures that we don't run into 'Address already in use' errors
+int reuse = 1;
+if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
+{
+	printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+	return 1;
+}
+
+struct sockaddr_in serv_addr = {
+	.sin_family = AF_INET,
+	.sin_port = htons(4221),
+	.sin_addr = {htonl(INADDR_ANY)},
+};
+
+if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+{
+	printf("Bind failed: %s \n", strerror(errno));
+	return 1;
+}
+
+int connection_backlog = 5;
+if (listen(server_fd, connection_backlog) != 0)
+{
+	printf("Listen failed: %s \n", strerror(errno));
+	return 1;
+}
+
+printf("Waiting for a client to connect...\n");
+client_addr_len = sizeof(client_addr);
+
+// while loop for handling concurrent connections
+while (1)
+{
+	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len); // create file descriptor for client
+	printf("Client connected\n");
+
+	if (0 == fork()) // create fork for handling multiple clients concurrently
 	{
-		char NOT_FOUND[30];
-		char OK[30];
-	};
+		close(server_fd);
 
-	struct HTTP_STATUS_MESSAGES HTTP_status_messages = {
-		"404 Not Found",
-		"200 OK"};
+		// declare buffer for client message
+		char client_buffer[2048];
 
-	struct Request
-	{
-		char http_method[10];
-		char path[100];
-		char http_protocol[10];
-	};
+		if (read(client_fd, client_buffer, sizeof(client_buffer)) < 0)
+		{ // read from client
+			printf("Error reading from client: %s\n", strerror(errno));
+			return -1;
+		}
 
-	struct Request request;
+		// printf("Printing client buffer\n%s\n", client_buffer);
 
-	struct Response
-	{
-		char http_protocol[10];
-		char status_code[30];
-		char status_message[30];
-		char content_type[30];
-		char content_length[10];
-		char body[1000];
-	};
+		char responseStr[1024];
 
-	struct Response response = {
-		.http_protocol = "HTTP/1.1"};
+		// If case for handling the different stages
 
-	// Disable output buffering
-	setbuf(stdout, NULL);
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
-
-	// Uncomment this block to pass the first stage
-
-	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1)
-	{
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-
-	// Since the tester restarts your program quite often, setting REUSE_PORT
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
-	{
-		printf("SO_REUSEPORT failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	struct sockaddr_in serv_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(4221),
-		.sin_addr = {htonl(INADDR_ANY)},
-	};
-
-	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
-	{
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0)
-	{
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
-
-	// while loop for handling concurrent connections
-	while (1)
-	{
-		int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len); // create file descriptor for client
-		printf("Client connected\n");
-
-		if (0 == fork()) // create fork for handling multiple clients concurrently
+		if (strstr(client_buffer, "GET / ") != NULL) // GET nothing
 		{
-			close(server_fd);
+			sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_OK, CRLF);
+			send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+			printf("Response:\n%s\n", responseStr);
+		}
+		else if (strstr(client_buffer, "/echo/") != NULL) // GET Echo the message
+		{
+			// extract the text after /ecjo/ and before HTTP/1.1
+			char *posStart = strstr(client_buffer, "/echo/") + strlen("/echo/");
+			char *posEnd = strstr(posStart, " HTTP/1.1");
+			size_t len = posEnd - posStart;
+			char body[len];
+			printf("len: %zu\n", len);
 
-			// declare buffer for client message
-			char client_buffer[2048];
+			strncpy(body, posStart, len);
+			body[len] = '\0';
+			printf("body: %s\n", body);
 
-			if (read(client_fd, client_buffer, sizeof(client_buffer)) < 0)
-			{ // read from client
-				printf("Error reading from client: %s\n", strerror(errno));
-				return -1;
-			}
+			// create response string
+			sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
+					HTTP_status_codes.HTTP_OK,
+					"text/plain",
+					CRLF,
+					len, // content length
+					CRLF, CRLF,
+					body, // content body
+					CRLF, CRLF, CRLF);
 
-			// printf("Printing client buffer\n%s\n", client_buffer);
+			// send response to client
+			printf("Response:\n%s\n", responseStr);				  // debug print
+			send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+		}
+		else if (strstr(client_buffer, "GET /user-agent") != NULL) // Get user agent
+		{
+			// extract the text after /ecjo/ and before HTTP/1.1
+			char *posStart = strstr(client_buffer, "User-Agent: ") + strlen("User-Agent: ");
+			char *posEnd = strstr(posStart, CRLF);
+			size_t len = posEnd - posStart;
+			char body[len];
+			printf("len: %zu\n", len);
 
-			char responseStr[1024];
+			strncpy(body, posStart, len);
+			body[len] = '\0';
+			printf("body: %s\n", body);
 
-			// If case for handling the different stages
+			// create response string
+			sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
+					HTTP_status_codes.HTTP_OK,
+					"text/plain",
+					CRLF,
+					len, // content length
+					CRLF, CRLF,
+					body, // content body
+					CRLF, CRLF, CRLF);
 
-			if (strstr(client_buffer, "GET / ") != NULL) // GET nothing
+			// send response to client
+			printf("Response:\n%s\n", responseStr);				  // debug print
+			send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+		}
+		else if (strstr(client_buffer, "GET /files/") != NULL) // Get a file
+		{
+			// extract file path from client buffer
+			char *posStart = strstr(client_buffer, "GET /files/") + strlen("GET /files/");
+			char *posEnd = strstr(posStart, " HTTP/1.1");
+			size_t len = posEnd - posStart;
+			char fileName[len];
+			strncpy(fileName, posStart, len);
+			fileName[len] = '\0';
+
+			fflush(stdout);
+			printf("File name: %s\n", fileName);
+
+			char completeFilePath[strlen(directory) + strlen(fileName)];
+			strcpy(completeFilePath, directory);
+			strncpy(completeFilePath + strlen(directory), fileName, strlen(fileName));
+			completeFilePath[strlen(directory) + strlen(fileName)] = '\0';
+			printf("Complete file path: %s\n", completeFilePath);
+
+			FILE *file = fopen(completeFilePath, "r");
+
+			// construct the full file path
+			if (file != NULL) // check if file exists in directory
 			{
-				sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_OK, CRLF);
-				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
-				printf("Response:\n%s\n", responseStr);
-			}
-			else if (strstr(client_buffer, "/echo/") != NULL) //GET Echo the message
-			{
-				// extract the text after /ecjo/ and before HTTP/1.1
-				char *posStart = strstr(client_buffer, "/echo/") + strlen("/echo/");
-				char *posEnd = strstr(posStart, " HTTP/1.1");
-				size_t len = posEnd - posStart;
-				char body[len];
-				printf("len: %zu\n", len);
+				printf("File exists\n");
+				fseek(file, 0, SEEK_END);
+				long fileSize = ftell(file);
+				rewind(file);
 
-				strncpy(body, posStart, len);
-				body[len] = '\0';
-				printf("body: %s\n", body);
+				char body[fileSize];
+				size_t bodyLen = fread(body, 1, fileSize, file);
+				body[bodyLen] = '\0';
+
+				fclose(file);
 
 				// create response string
 				sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
 						HTTP_status_codes.HTTP_OK,
-						"text/plain",
+						"application/octet-stream",
 						CRLF,
-						len, // content length
+						bodyLen, // content length
 						CRLF, CRLF,
 						body, // content body
 						CRLF, CRLF, CRLF);
@@ -175,110 +256,88 @@ int main(int argc, char *argv[])
 				printf("Response:\n%s\n", responseStr);				  // debug print
 				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
 			}
-			else if (strstr(client_buffer, "GET /user-agent") != NULL) //Get user agent
-			{
-				// extract the text after /ecjo/ and before HTTP/1.1
-				char *posStart = strstr(client_buffer, "User-Agent: ") + strlen("User-Agent: ");
-				char *posEnd = strstr(posStart, CRLF);
-				size_t len = posEnd - posStart;
-				char body[len];
-				printf("len: %zu\n", len);
 
-				strncpy(body, posStart, len);
-				body[len] = '\0';
-				printf("body: %s\n", body);
-
-				// create response string
-				sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
-						HTTP_status_codes.HTTP_OK,
-						"text/plain",
-						CRLF,
-						len, // content length
-						CRLF, CRLF,
-						body, // content body
-						CRLF, CRLF, CRLF);
-
-				// send response to client
-				printf("Response:\n%s\n", responseStr);				  // debug print
-				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
-			}
-			else if (strstr(client_buffer, "GET /files/") != NULL) //Get a file
-			{
-				// extract file path from client buffer
-				char *posStart = strstr(client_buffer, "GET /files/") + strlen("GET /files/");
-				char *posEnd = strstr(posStart, " HTTP/1.1");
-				size_t len = posEnd - posStart;
-				char fileName[len];
-				strncpy(fileName, posStart, len);
-				fileName[len] = '\0';
-
-				fflush(stdout);
-				printf("File name: %s\n", fileName);
-
-				char completeFilePath[strlen(directory) + strlen(fileName)];
-				strcpy(completeFilePath, directory);
-				strncpy(completeFilePath + strlen(directory), fileName, strlen(fileName));
-				completeFilePath[strlen(directory) + strlen(fileName)] = '\0';
-				printf("Complete file path: %s\n", completeFilePath);
-
-				FILE *file = fopen(completeFilePath, "r");
-
-				// construct the full file path
-				if (file != NULL) // check if file exists in directory
-				{
-					printf("File exists\n");
-					fseek(file, 0, SEEK_END);
-					long fileSize = ftell(file);
-					rewind(file);
-
-					char body[fileSize];
-					size_t bodyLen = fread(body, 1, fileSize, file);
-					body[bodyLen] = '\0';
-
-					fclose(file);
-
-					// create response string
-					sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
-							HTTP_status_codes.HTTP_OK,
-							"application/octet-stream",
-							CRLF,
-							bodyLen, // content length
-							CRLF, CRLF,
-							body, // content body
-							CRLF, CRLF, CRLF);
-
-					// send response to client
-					printf("Response:\n%s\n", responseStr);				  // debug print
-					send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
-				}
-
-				else
-				{
-					sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_NOT_FOUND, CRLF);
-					send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
-					printf("Response:\n%s\n", responseStr);
-				}
-			}
-			else if (strstr(client_buffer, "POST ") != NULL) //POST a file
-			{
-				printf("POST request\n");
-			}
 			else
 			{
 				sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_NOT_FOUND, CRLF);
 				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
 				printf("Response:\n%s\n", responseStr);
 			}
+		}
+		else if (strstr(client_buffer, "POST ") != NULL) // POST a file
+		{
+			printf("POST request\n");
+			// extract file path from client buffer
+			char *posStart = strstr(client_buffer, "POST /files/") + strlen("POST /files/");
+			char *posEnd = strstr(posStart, " HTTP/1.1");
+			size_t len = posEnd - posStart;
+			char fileName[len];
+			strncpy(fileName, posStart, len);
+			fileName[len] = '\0';
 
-			exit(0);
+			fflush(stdout);
+			printf("File name: %s\n", fileName);
+
+			char completeFilePath[strlen(directory) + strlen(fileName)];
+			strcpy(completeFilePath, directory);
+			strncpy(completeFilePath + strlen(directory), fileName, strlen(fileName));
+			completeFilePath[strlen(directory) + strlen(fileName)] = '\0';
+			printf("Complete file path: %s\n", completeFilePath);
+
+			/*FILE *file = fopen(completeFilePath, "r");
+
+			// construct the full file path
+			if (file != NULL) // check if file exists in directory
+			{
+				printf("File exists\n");
+				fseek(file, 0, SEEK_END);
+				long fileSize = ftell(file);
+				rewind(file);
+
+				char body[fileSize];
+				size_t bodyLen = fread(body, 1, fileSize, file);
+				body[bodyLen] = '\0';
+
+				fclose(file);
+
+				// create response string
+				sprintf(responseStr, "%sContent-Type: %s%sContent-Length: %zu%s%s%s%s%s%S",
+						HTTP_status_codes.HTTP_OK,
+						"application/octet-stream",
+						CRLF,
+						bodyLen, // content length
+						CRLF, CRLF,
+						body, // content body
+						CRLF, CRLF, CRLF);
+
+				// send response to client
+				printf("Response:\n%s\n", responseStr);				  // debug print
+				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+			}
+
+			else
+			{
+				sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_NOT_FOUND, CRLF);
+				send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+				printf("Response:\n%s\n", responseStr);
+			}*/
 		}
 		else
 		{
-			close(client_fd);
+			sprintf(responseStr, "%s%s", HTTP_status_codes.HTTP_NOT_FOUND, CRLF);
+			send(client_fd, responseStr, sizeof(responseStr), 0); // send response to client
+			printf("Response:\n%s\n", responseStr);
 		}
+
+		exit(0);
 	}
+	else
+	{
+		close(client_fd);
+	}
+}
 
-	close(server_fd);
+close(server_fd);
 
-	return 0;
+return 0;
 }
