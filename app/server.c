8e236651c067a39e6c10737cd023de912ff0207e
Jens Thieme Almkvist <jens.thieme-almkvist@br-automation.com>
2024-04-18 11:58:39 +0200

    testing

--- app/server.c
+++ app/server.c
@@ -11,15 +11,12 @@ int main(int argc, char *argv[])
 {
 	int argcTest = argc;
 
+	printf("argc: %d\n", argc);
+
 	printf("argcTest: %d\n", argcTest);
 	// printing the arguments for main
 
-	    int i;
-    printf("%d\n",argc);
-    for(i=0;i<argc-1;i++)
-    {
-        printf("%s",argv[i]);
-    }
+
 
 	char CRLF[4] = "\r\n"; // carriage return line feed
 	// variable declarations
